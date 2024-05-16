249405ded13f05270dc0ce1a6029ec2c73afebee
PucLucasCampos <lucas.cr16@puccampinas.edu.br>
2024-05-16 15:33:16 -0300

    Refatoramento do codigo: Inseri as funções Cadastrar_funcionarios, Validar_codigo_funcao, Validar_id_matricula, Calcular_imposto, Remover_funcionario, Folha_pagamento (Não finalizada contem erros de logica.)

--- main.py
+++ main.py
@@ -1,37 +1,100 @@
 import os
 
-# funcionarios = {
-#     matricula: [nome, codigo_função, numero_faltas_mes, salario_bruto]
-# }
 funcionarios = {}
 
-def limpar_tela():
-    os.system('cls')
+def Limpar():
+    os.system("cls")
 
-def menu():
-    menu = int(input("""
-    [1]. INSERIR FUNCIONARIO
-    [2]. REMOVER FUNCIONARIO
-    [3]. FOLHA PAGAMENTO
-    [4]. RELATORIO FUNCIONARIO
-    [5]. MAIOR SALARIO LIQUIDO
-    [6]. MAIOR NUMERO DE FALTAS NO MES 
-    """))
-    return menu
-
-def inserir_func():
-    opcao = menu()
-    if opcao == 1:
-        matricula = int(input("Matricula: "))
-        nome = str(input("Nome: "))
+def Validar_codigo_funcao():
+    cod_funcao = int(input("Código da função: "))
+    while cod_funcao != 101 and cod_funcao != 102:
+        print("Código da função invalido >>> Tente novamente!!! ")
         cod_funcao = int(input("Código da função: "))
-        while cod_funcao != 101 and cod_funcao != 102:
-            print("Código da funçõa invalido >>> Tente novamente!!! ")
-            cod_funcao = int(input("Código da função: "))
-        numero_faltas = int(input("Número de faltas por mês: "))
-        salario_bruto = float(input("Salario Bruto: "))
-        funcionarios[matricula] = [nome, cod_funcao, numero_faltas, salario_bruto]
+    return cod_funcao
+
+def Validar_id_matricula():
+    id_matricula = int(input("Digite o ID da matrícula:"))
+    while id_matricula in funcionarios:
+        print("ID de matricula ja existe >>> Tente novamente!!! ")
+        id_matricula = int(input("Digite o ID da matrícula:"))
+    return id_matricula
+
+def Calculo_imposto(salario_bruto):
+    if salario_bruto <= 2259.20:
+        imposto = 0
+    elif salario_bruto <= 2828.65:
+        imposto = 0.075
+    elif salario_bruto <= 3751.05:
+        imposto = 0.15
+    elif salario_bruto <= 4664.68:
+        imposto = 0.225 
     else:
-        print("opção invalida")
-    
-inserir_func()
+        imposto = 0.275
+    return imposto
+
+
+def Cadastro_funcionario():
+    id_matricula = Validar_id_matricula()
+    nome = str(input("Nome do funcionario: "))
+    print("[101] - Vendedor\n[102] - Administrativo")
+    cod_funcao = Validar_codigo_funcao()
+    if cod_funcao == 101:
+        volume_vendas = int(input("Volume de vendas: "))
+        salario_bruto = (volume_vendas * 0.9) + 1500
+        print(salario_bruto)
+        imposto = 0
+    else:
+        print("Salário varia entre R$2150,00 até R$6950,00")
+        salario_bruto = float(input("Salário do funcionário: "))
+        while salario_bruto < 2150 or salario_bruto > 6950:
+            print("Por favor, digite um salário dentro da faixa especificada.")
+            salario_bruto = float(input("Salário do funcionário: "))
+    numero_faltas = int(input("Número de faltas por mês: "))
+    for i in range(numero_faltas):
+        salario_bruto -= 1500/30
+    imposto = Calculo_imposto(salario_bruto)
+
+    funcionarios[id_matricula] = [nome, cod_funcao, numero_faltas, salario_bruto, imposto]
+
+def Remover_funcionario():
+    print(">>> Remoção de Funcionario >>>")
+    remover = int(input("ID Matriucla do funcionário: "))
+    while remover not in funcionarios:
+        print("ID de matricula não existe >>> Tente novamente!!!")
+        remover = int(input("ID Matriucla do funcionário: "))
+    del funcionarios[(remover)]
+
+def Folha_pagamento():
+    print("Determinar folha de pagamento por ID de determinado funcionário")
+    key = int(input("ID do(a) funcionário: "))
+    for achar in funcionarios.keys(): # ERRO: esta pegandon todos os funcionarios 
+        if key == achar:
+            for id,index in funcionarios.items():
+                print(f"""
+                ID: {id}
+                NOME: {index[0]}
+                CODIGO: {index[1]}
+                SALÁRIO BRUTO: R${index[3]}
+                PORCENTUAL DE IMPOSTO: {index[4]}
+                """)
+
+menu = 1
+while menu > 0:
+    menu = int(input("""
+       |================================|
+       | [1] - Adicionar funcionários.  |
+       |                                |
+       | [2] - Remover funcionários.    |
+       |                                |
+       | [3] - Folha de pagamento por ID|
+       |                                |
+       | [0] - Sair do programa         |
+       |================================|
+                    OPÇÃO:"""))
+    Limpar()
+    if menu == 1:
+        Cadastro_funcionario()
+    elif menu == 2:
+        Remover_funcionario()
+    elif menu == 3:
+        Folha_pagamento()
